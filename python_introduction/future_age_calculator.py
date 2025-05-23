4323634ba8582f8271adda06eaf732177b6819c3
Juliet Duru <julietduru78@gmail.com>
2025-05-23 12:54:39 -0700

    Fix user input prompt to match checker requirements

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,5 +1,5 @@
 #Prompt the user for their current age
-current_age = 30
+current_age = int(input("what is your current age? "))
 #Calculate age in 2050 (27 years from 2023)
 age_in_2050 = current_age + 27
 #Print the result
