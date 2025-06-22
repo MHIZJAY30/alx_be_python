384dbfad4a2d1a11315a854a6b72c1f686b0dec3
Juliet Duru <julietduru78@gmail.com>
2025-06-21 21:19:19 -0700

    Fix: Add the correct output for book_class

--- oop/book_class.py
+++ oop/book_class.py
@@ -3,7 +3,6 @@ class Book:
         self.title = title
         self.author = author
         self.year = year
-        print(f"Book created: {self}")
 
     def __del__(self):
         print(f"Deleting '{self.title}'")
